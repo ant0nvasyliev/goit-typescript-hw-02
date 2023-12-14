6fab93be7b4f6cb3a5a344c14906bdc1be1a558d
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-12-14 13:07:52 +0000

    1

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProp {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponentProp> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<ComponentProp> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
